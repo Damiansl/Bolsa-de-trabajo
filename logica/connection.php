47ee7259e04040c93c2d12b08cd7247b96b57f24
Damián Sala <79192538+Damiansl@users.noreply.github.com>
2024-10-18 09:40:32 -0300

    Update connection.php

--- logica/connection.php
+++ logica/connection.php
@@ -4,7 +4,7 @@ class conecction{
 
     function conectar(){
         //$pdo = new PDO('mysql:host=localhost;id22089797_bolsadetrabajotecncia2','id22089797_bdtt2','WebHostTecnica2JuninEEST2!');
-        $pdo= mysqli_connect('sql206.infinityfree.com', 'if0_37185014', 'iLxdXLKRZX3IX', 'if0_37185014_bolsa_de_trabajo');
+        $pdo= mysqli_connect('localhost', 'worpre', '1234', 'bolsadetrabajo');
         return $pdo;
     }
 
